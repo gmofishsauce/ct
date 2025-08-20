8cd3a7ffef7692bb6375e99ced8fcff46dfa0fa8
gmofishsauce <pdxjjb@gmail.com>
2025-08-20 15:57:28 -0700

    Working toy example of three.js graphics

--- js/main.js
+++ js/main.js
@@ -1,23 +1,76 @@
 import * as THREE from 'three';
 
-const scene = new THREE.Scene();
-const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+function main() {
 
-const renderer = new THREE.WebGLRenderer();
-renderer.setSize( window.innerWidth, window.innerHeight );
-document.body.appendChild( renderer.domElement );
+    const canvas = document.querySelector( '#c' );
+    const renderer = new THREE.WebGLRenderer( { antialias: true, canvas } );
+    const scene = new THREE.Scene();
 
-const geometry = new THREE.BoxGeometry( 1, 1, 1 );
-const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
-const cube = new THREE.Mesh( geometry, material );
-scene.add( cube );
+    const fov = 75;
+    const aspect = 2; // the canvas default
+    const near = 0.1;
+    const far = 5;
+    const camera = new THREE.PerspectiveCamera( fov, aspect, near, far );
+    camera.position.z = 2;
 
-camera.position.z = 5;
+    const lightColor = 0xFFFFFF;
+    const lightIntensity = 3;
+    const light = new THREE.DirectionalLight(lightColor, lightIntensity);
+    light.position.set(-1, 2, 4);
+    scene.add(light);
 
-function animate() {
-  cube.rotation.x += 0.01;
-  cube.rotation.y += 0.01;
-  renderer.render( scene, camera );
+    const boxWidth = 1;
+    const boxHeight = 1;
+    const boxDepth = 1;
+    const geometry = new THREE.BoxGeometry( boxWidth, boxHeight, boxDepth );
+
+	function makeInstance( geometry, color, x ) {
+		const material = new THREE.MeshPhongMaterial( { color } );
+		const cube = new THREE.Mesh( geometry, material );
+		scene.add( cube );
+		cube.position.x = x;
+		return cube;
+	}
+
+    const cubes = [
+      makeInstance(geometry, 0x44aa88,  0),
+      makeInstance(geometry, 0x8844aa, - 2),
+      makeInstance(geometry, 0xaa8844,  2),
+    ];
+
+    function resizeRendererToDisplaySize(renderer) {
+      const canvas = renderer.domElement;
+      const width = canvas.clientWidth;
+      const height = canvas.clientHeight;
+      const needResize = canvas.width !== width || canvas.height !== height;
+      if (needResize) {
+        renderer.setSize(width, height, false);
+      }
+      return needResize;
+    }
+
+    function render( time ) {
+      time *= 0.001;  // convert time to seconds
+
+      if (resizeRendererToDisplaySize(renderer)) {
+        const canvas = renderer.domElement;
+        camera.aspect = canvas.clientWidth / canvas.clientHeight;
+        camera.updateProjectionMatrix();
+      }
+
+      cubes.forEach((cube, ndx) => {
+        const speed = 1 + ndx * .1;
+        const rot = time * speed;
+        cube.rotation.x = rot;
+        cube.rotation.y = rot;
+      });
+
+      renderer.render( scene, camera );
+      requestAnimationFrame( render );
+    }
+
+    requestAnimationFrame( render );
+
 }
-renderer.setAnimationLoop( animate );
 
+main();
